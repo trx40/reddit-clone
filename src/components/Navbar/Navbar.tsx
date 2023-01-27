39a61e0f0196ff77ecb1887b1412674d32ac73e4
trx40 <trm2468@gmail.com>
2023-01-27 20:42:48 +0530

    Update Navbar.tsx
    Add Logos to the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,26 @@
+//  Flex component is a div from Chakra UI which has flex 
+//  properties enabled
+
+import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
-
-
 
 const Navbar:React.FC = () => {
     
     return (
-        <nav></nav>
+        <Flex bg="white" height="44px" padding="6px 12px">
+            <Flex align="center">
+                <Image alt="reddit-face" 
+                    src="/images/redditFace.svg" 
+                    height="30px" />
+                <Image alt="reddit-text" 
+                    src="/images/redditText.svg" 
+                    height="46px" 
+                    display={{ base: "none", md: "unset"}} />
+            </Flex>
+           {/* <Directory />
+           <SearchInput />
+           <RightContent />  */}
+        </Flex>
     )
 }
 export default Navbar;
