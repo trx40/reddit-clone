e0d8013c04e2a33357cf0a96f4efe3c7e09658c0
trx40 <trm2468@gmail.com>
2023-01-27 20:41:04 +0530

    Debug global styles and colors

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,26 +1,29 @@
 // 1. Import `extendTheme`
 import { extendTheme } from "@chakra-ui/react"
+import type { StyleFunctionProps } from '@chakra-ui/styled-system'
 import '@fontsource/open-sans/300.css'
 import '@fontsource/open-sans/400.css'
 import '@fontsource/open-sans/700.css'
 // 2. Call `extendTheme` and pass your custom values
 export const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.200',
+      },
+    },
+},
   colors: {
-    brand: {
-      100: "#FF3C00",
-    },
-    fonts: {
-        body: "Open sans, sans-serif",
-    },
-    styles: {
-        global: () => ({
-          body: {
-            bg: "gray.200",
-          },
-        }),
-    },
-    components: {
+    
+   brand: {
+    100: "#FF3C00",
+  },
+  fonts: {
+    body: "Open sans, sans-serif",
+  },
+    
+   components: {
       // Button
-    },
+  },
   },
 })
