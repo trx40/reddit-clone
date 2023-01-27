9d9907fb838e6af401ae5eefef38319073e9bc98
trx40 <trm2468@gmail.com>
2023-01-27 22:59:55 +0530

    Add SearchInput to Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@
 
 import { Flex, Image } from '@chakra-ui/react';
 import React from 'react';
+import SearchInput from './SearchInput';
 
 const Navbar:React.FC = () => {
     
@@ -17,8 +18,8 @@ const Navbar:React.FC = () => {
                     height="46px" 
                     display={{ base: "none", md: "unset"}} />
             </Flex>
-           {/* <Directory />
            <SearchInput />
+           {/* <Directory />
            <RightContent />  */}
         </Flex>
     )
--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,17 +1,38 @@
-import { Flex } from '@chakra-ui/react';
-import { Input } from '@chakra-ui/react';
-import React from 'react';
+import { SearchIcon } from "@chakra-ui/icons";
+import { Flex, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import React from "react";
 
 type SearchInputProps = {
-    // user: 
+  // user:
+  // children: any,
 };
 
-const SearchInput:React.FC<SearchInputProps> = () => {
-    
-    return (
-        <Flex>
-            <Input placeholder='Search' />
-        </Flex>
-    )
-}
+const SearchInput: React.FC<SearchInputProps> = () => {
+  return (
+    <Flex flexGrow={1} mr={2} align='center'>
+      <InputGroup>
+        <InputLeftElement pointerEvents='none'>
+          <SearchIcon color='gray.400' mb={1} />
+        </InputLeftElement>
+        <Input
+          placeholder='Search Reddit'
+          fontSize='10pt'
+          _placeholder={{ color: "gray.500" }}
+          _hover={{
+            bg: "white",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+          _focus={{
+            outline: "none",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+          height='34px'
+          bg='gray.50'
+        />
+      </InputGroup>
+    </Flex>
+  );
+};
 export default SearchInput;
