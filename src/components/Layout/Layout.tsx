f6315e567d8ae9c97aa8e5f34cb724a2af7e3a98
trx40 <trm2468@gmail.com>
2023-01-28 17:18:06 +0530

    Update type and imports

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,20 +1,16 @@
-
-
-import React from 'react';
-import Navbar from '../Navbar/Navbar';
+import React, { ReactNode } from "react";
+import Navbar from "../Navbar/Navbar";
 
 type LayoutProps = {
-    children: any
+  children: ReactNode;
 };
 
-const Layout:React.FC<LayoutProps> = ({children}) => {
-    
-    return (
-        <>
-         <Navbar />
-            <main>{children}</main>
-        </>
-    );
-    
-}
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+};
 export default Layout;
