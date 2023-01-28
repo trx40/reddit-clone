b8bb63e822d443d5d191372faabff72c101b2371
trx40 <trm2468@gmail.com>
2023-01-28 23:37:57 +0530

    Update AuthModal.tsx
    Close the auth modal after successfully signing in.
    useEffect to watch 'user'(from firebase auth hook) and close modal

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -5,8 +5,10 @@
 // So we require a Global state. Which will allow us to open this Modal
 // from anywhere in the app.
 import { authModalState } from "@/src/atoms/authModalAtom";
+import { auth } from "@/src/firebase/clientApp";
 import {
   Flex,
+  Text,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -14,18 +16,27 @@ import {
   ModalHeader,
   ModalOverlay,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilState } from "recoil";
 import AuthInputs from "./AuthInputs";
+import OAuthButtons from "./OAuthButtons";
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
+  const [user, loading, error] = useAuthState(auth);
 
   const handleClose = () => {
     setModalState((prev) => ({ ...prev, open: false }));
     //enclosed in (parantheses) as parser cant recognize
     //object literal vs. function definition
   };
+
+  useEffect(() => {
+    if (user) handleClose();
+    console.log("user", user);
+  }, [user]);
+
   return (
     // We use <> </> React fragments to avoid <div> wrappers
     <>
@@ -51,7 +62,10 @@ const AuthModal: React.FC = () => {
               align='center'
               width='70%'
             >
-              {/* <OAuthButtons /> */}
+              <OAuthButtons />
+              <Text fontWeight={700} color='gray.500'>
+                OR
+              </Text>
               <AuthInputs />
               {/* <ResetPassword /> */}
             </Flex>
