26091f45c89be1f728c318e4d5c4fbb459c0a161
trx40 <trm2468@gmail.com>
2023-01-28 11:53:59 +0530

    Import RightContent component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,27 @@
-//  Flex component is a div from Chakra UI which has flex 
+//  Flex component is a div from Chakra UI which has flex
 //  properties enabled
 
-import { Flex, Image } from '@chakra-ui/react';
-import React from 'react';
-import SearchInput from './SearchInput';
+import { Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import RightContent from "./RightContent/RightContent";
+import SearchInput from "./SearchInput";
 
-const Navbar:React.FC = () => {
-    
-    return (
-        <Flex bg="white" height="44px" padding="6px 12px">
-            <Flex align="center">
-                <Image alt="reddit-face" 
-                    src="/images/redditFace.svg" 
-                    height="30px" />
-                <Image alt="reddit-text" 
-                    src="/images/redditText.svg" 
-                    height="46px" 
-                    display={{ base: "none", md: "unset"}} />
-            </Flex>
-           <SearchInput />
-           {/* <Directory />
-           <RightContent />  */}
-        </Flex>
-    )
-}
+const Navbar: React.FC = () => {
+  return (
+    <Flex bg='white' height='44px' padding='6px 12px'>
+      <Flex align='center'>
+        <Image alt='reddit-face' src='/images/redditFace.svg' height='30px' />
+        <Image
+          alt='reddit-text'
+          src='/images/redditText.svg'
+          height='46px'
+          display={{ base: "none", md: "unset" }}
+        />
+      </Flex>
+      <SearchInput />
+      {/* <Directory /> */}
+      <RightContent />
+    </Flex>
+  );
+};
 export default Navbar;
