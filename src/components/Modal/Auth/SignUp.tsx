5d2ece5dc2113bea73dbadae5de805f56e70eb87
trx40 <trm2468@gmail.com>
2023-01-28 23:34:37 +0530

    Update SignUp.tsx
    Implement sign up using firebase hooks and handle auth errors

--- src/components/Modal/Auth/SignUp.tsx
+++ src/components/Modal/Auth/SignUp.tsx
@@ -1,20 +1,38 @@
 import { authModalState } from "@/src/atoms/authModalAtom";
-import { Input, Button, Flex, Text } from "@chakra-ui/react";
+import { auth } from "@/src/firebase/clientApp";
+import { FIREBASE_ERRORS } from "@/src/firebase/errors";
+import { Button, Flex, Input, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useSetRecoilState } from "recoil";
-
 const SignUp: React.FC = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
-  const [formData, setFormData] = useState({
+  const [signUpForm, setSignUpForm] = useState({
     email: "",
     password: "",
+    confirmPassword: "",
   });
+  const [formError, setFormError] = useState("");
 
-  const onSubmit = () => {};
+  const [createUserWithEmailAndPassword, user, loading, userError] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(userError?.message);
+    // Remove error after subsequent submit
+    if (formError) setFormError("");
+    if (signUpForm.password !== signUpForm.confirmPassword) {
+      // set formError
+      setFormError("Passwords do not match");
+      return;
+    }
+    createUserWithEmailAndPassword(signUpForm.email, signUpForm.password);
+  };
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     //update form state
-    setFormData((prev) => ({
+    setSignUpForm((prev) => ({
       ...prev,
       [event.target.name]: event.target.value,
     }));
@@ -68,7 +86,7 @@ const SignUp: React.FC = () => {
       />
       <Input
         required
-        name='password'
+        name='confirmPassword'
         type='password'
         placeholder='Confirm password'
         mb={2}
@@ -88,11 +106,24 @@ const SignUp: React.FC = () => {
         }}
         bg='gray.50'
       />
-      <Button width='100%' height='36px' mb={2} mt={2} type='submit'>
+      {
+        <Text textAlign='center' fontSize='10pt' color='red'>
+          {formError || (userError && FIREBASE_ERRORS[userError.message])}
+        </Text>
+      }
+
+      <Button
+        width='100%'
+        height='36px'
+        mb={2}
+        mt={2}
+        type='submit'
+        isLoading={loading}
+      >
         Sign Up
       </Button>
       <Flex fontSize='9pt' justifyContent='center'>
-        <Text mr={1}>Already a user?</Text>
+        <Text mr={1}>Already a redditor?</Text>
         <Text
           color='blue.500'
           fontWeight={700}
