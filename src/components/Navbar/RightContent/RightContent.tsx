e185f9b7cb015648b1b1c8fd6e65158541ee5b40
trx40 <trm2468@gmail.com>
2023-01-28 23:41:06 +0530

    Update RightContent.tsx
    Accept user as a prop with type definition.
    If user exists -> show logout button or show Auth buttons(login & signup)

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,18 +1,26 @@
-import { Flex } from "@chakra-ui/react";
+import { auth } from "@/src/firebase/clientApp";
+import { Button, Flex } from "@chakra-ui/react";
+import { signOut } from "firebase/auth";
 import React from "react";
 import AuthModal from "../../Modal/Auth/AuthModal";
 import AuthButtons from "./AuthButtons";
 
 type RightContentProps = {
-  // user: any,
+  user: any;
 };
 
-const RightContent: React.FC<RightContentProps> = () => {
+const RightContent: React.FC<RightContentProps> = ({ user }) => {
   return (
     <>
       <AuthModal />
       <Flex justify='center' align='center'>
-        <AuthButtons />
+        {user ? (
+          <Button variant='outline' onClick={() => signOut(auth)}>
+            Logout
+          </Button>
+        ) : (
+          <AuthButtons />
+        )}
       </Flex>
     </>
   );
