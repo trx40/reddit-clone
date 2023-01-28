689f0d74077da1fb0aa628ebe156e462e2aa9e1e
trx40 <trm2468@gmail.com>
2023-01-28 12:06:06 +0530

    Update _app.tsx
    Add Recoil for global state management to the App root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
-import {theme} from '../chakra/theme'
-import Layout from '../components/Layout/Layout'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil"; //global state management
+import { theme } from "../chakra/theme";
+import Layout from "../components/Layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
-  )
+    <RecoilRoot>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </RecoilRoot>
+  );
 }
