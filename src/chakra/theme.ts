eadc1cec0f90f64ad9e077ff99b27cafceb088c9
trx40 <trm2468@gmail.com>
2023-01-28 11:50:01 +0530

    Update theme.ts
    Add button styling to the chakra global theme

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -4,6 +4,7 @@ import type { StyleFunctionProps } from '@chakra-ui/styled-system'
 import '@fontsource/open-sans/300.css'
 import '@fontsource/open-sans/400.css'
 import '@fontsource/open-sans/700.css'
+import { Button } from './button'
 // 2. Call `extendTheme` and pass your custom values
 export const theme = extendTheme({
   styles: {
@@ -12,18 +13,16 @@ export const theme = extendTheme({
         bg: 'gray.200',
       },
     },
-},
+  },
   colors: {
-    
-   brand: {
-    100: "#FF3C00",
+    brand: {
+      100: "#FF3C00",
+    },
   },
   fonts: {
     body: "Open sans, sans-serif",
-  },
-    
-   components: {
-      // Button
-  },
+  }, 
+  components: {
+    Button
   },
 })
