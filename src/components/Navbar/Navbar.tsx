46c891c1c24bb33c4e27de0213f4404e43fbb566
trx40 <trm2468@gmail.com>
2023-01-28 23:39:30 +0530

    Update Navbar.tsx
    use Auth state hook to get 'user' and manage state at Navbar level.
    Pass 'user' as prop wherever needed.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,15 @@
 //  Flex component is a div from Chakra UI which has flex
 //  properties enabled
 
+import { auth } from "@/src/firebase/clientApp";
 import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import RightContent from "./RightContent/RightContent";
 import SearchInput from "./SearchInput";
 
 const Navbar: React.FC = () => {
+  const [user, loading, error] = useAuthState(auth);
   return (
     <Flex bg='white' height='44px' padding='6px 12px'>
       <Flex align='center'>
@@ -20,7 +23,7 @@ const Navbar: React.FC = () => {
       </Flex>
       <SearchInput />
       {/* <Directory /> */}
-      <RightContent />
+      <RightContent user={user} />
     </Flex>
   );
 };
