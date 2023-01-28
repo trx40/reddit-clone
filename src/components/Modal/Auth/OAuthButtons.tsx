864c82d739a6573d3dbb1978ed1de05cd7f00bb2
trx40 <trm2468@gmail.com>
2023-01-28 23:35:19 +0530

    Update OAuthButtons.tsx
    Implement OAuth with Google sign in and handle errors

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,10 +1,18 @@
-import { Button, Flex, Image } from "@chakra-ui/react";
+import { auth } from "@/src/firebase/clientApp";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
   return (
     <Flex direction='column' width='100%' mb={2}>
-      <Button variant='oauth' mb={2}>
+      <Button
+        variant='oauth'
+        mb={2}
+        isLoading={loading}
+        onClick={() => signInWithGoogle()}
+      >
         <Image
           alt='Google Logo'
           src='images/googlelogo.png'
@@ -14,6 +22,7 @@ const OAuthButtons: React.FC = () => {
         Continue with Google
       </Button>
       <Button variant='oauth'>Some other provider</Button>
+      {error && <Text>{error.message}</Text>}
     </Flex>
   );
 };
