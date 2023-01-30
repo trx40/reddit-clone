8f9b778beefe5f851dff012cc4d075b8657d7906
Tejas Mahajan <trm2468@gmail.com>
2023-01-31 00:00:03 +0530

    Update Navbar.tsx
    Responsive navbar length
    
    Add directory component and reorder SearchInput component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,14 +5,24 @@ import { auth } from "@/src/firebase/clientApp";
 import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Directory from "./Directory/Directory";
 import RightContent from "./RightContent/RightContent";
 import SearchInput from "./SearchInput";
 
 const Navbar: React.FC = () => {
   const [user, loading, error] = useAuthState(auth);
   return (
-    <Flex bg='white' height='44px' padding='6px 12px'>
-      <Flex align='center'>
+    <Flex
+      bg='white'
+      height='44px'
+      padding='6px 12px'
+      justify={{ md: "space-between" }}
+    >
+      <Flex
+        align='center'
+        width={{ base: "40px", md: "auto" }}
+        mr={{ base: 0, md: 2 }}
+      >
         <Image alt='reddit-face' src='/images/redditFace.svg' height='30px' />
         <Image
           alt='reddit-text'
@@ -21,8 +31,8 @@ const Navbar: React.FC = () => {
           display={{ base: "none", md: "unset" }}
         />
       </Flex>
-      <SearchInput />
-      {/* <Directory /> */}
+      <Directory />
+      <SearchInput user={user} />
       <RightContent user={user} />
     </Flex>
   );
