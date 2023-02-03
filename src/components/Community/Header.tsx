762369ac6452c0c67abd4aa0289617a9116ca9b8
Tejas Mahajan <trm2468@gmail.com>
2023-02-03 21:40:56 +0530

    Update Header.tsx
    Show community logo if present else show default snoo
    Show community name besides logo

--- src/components/Community/Header.tsx
+++ src/components/Community/Header.tsx
@@ -1,5 +1,5 @@
 import { Community } from "@/src/atoms/communitiesAtom";
-import { Box, Flex, Icon } from "@chakra-ui/react";
+import { Box, Button, Flex, Icon, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { FaReddit } from "react-icons/fa";
 
@@ -8,20 +8,36 @@ type HeaderProps = {
 };
 
 const Header: React.FC<HeaderProps> = ({ communityData }) => {
+  const isJoined = false; // read from our communitySnippets
   return (
     <Flex direction='column' width='100%' height='146px'>
       <Box height='50%' bg='blue.400' />
       <Flex justify='center' bg='white' flexGrow={1}>
         <Flex width='95%' maxWidth='860px' border='1px solid red'>
-          <Icon
-            as={FaReddit}
-            fontSize={64}
-            position='relative'
-            top={-3}
-            color='blue.500'
-            border='4px solid white'
-            borderRadius='50%'
-          />
+          {communityData.imageURL ? (
+            <Image alt='community logo' />
+          ) : (
+            <Icon
+              as={FaReddit}
+              fontSize={64}
+              position='relative'
+              top={-3}
+              color='blue.500'
+              border='4px solid white'
+              borderRadius='50%'
+            />
+          )}
+          <Flex padding='10px 16px'>
+            <Flex direction='column' mr={6}>
+              <Text fontWeight={800} fontSize='16pt'>
+                {communityData.id}
+              </Text>
+              <Text fontWeight={600} fontSize='10pt' color='gray.400'>
+                r/{communityData.id}
+              </Text>
+            </Flex>
+            <Button>{isJoined ? "Joined" : "Join"}</Button>
+          </Flex>
         </Flex>
       </Flex>
     </Flex>
