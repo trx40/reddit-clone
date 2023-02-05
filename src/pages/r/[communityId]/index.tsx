c54bd17d520a1cb9dd15b6fcde9d7cf843e3e670
trx40 <trm2468@gmail.com>
2023-02-05 23:04:03 +0530

    Update index.tsx
    Add PageContent component to display community posts and stats on finding the community in firebase

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,6 +1,7 @@
 import { Community } from "@/src/atoms/communitiesAtom";
 import Header from "@/src/components/Community/Header";
 import CommunityNotFound from "@/src/components/Community/NotFound";
+import PageContent from "@/src/components/Layout/PageContent";
 import { firestore } from "@/src/firebase/clientApp";
 import { doc, getDoc } from "firebase/firestore";
 import { GetServerSidePropsContext } from "next";
@@ -18,6 +19,14 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
   return (
     <>
       <Header communityData={communityData} />
+      <PageContent>
+        <>
+          <div>LHS</div>
+        </>
+        <>
+          <div>RHS</div>
+        </>
+      </PageContent>
     </>
   );
 };
