59b36f3a100e2212169f7ce62c97acebff746637
trx40 <trm2468@gmail.com>
2023-02-05 23:05:42 +0530

    Create PageContent.tsx
    Takes the children components as props and positions them. Children components here being the community feed and the community stats

---
+++ src/components/Layout/PageContent.tsx
@@ -0,0 +1,39 @@
+import { Flex } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+
+type PageContentProps = {
+  children: ReactNode;
+};
+
+const PageContent: React.FC<PageContentProps> = ({ children }) => {
+  return (
+    <Flex justify='center' p='16px 0px' border='1px solid red'>
+      <Flex
+        width='95%'
+        justify='center'
+        maxWidth='860px'
+        border='1px solid green'
+      >
+        {/* LHS */}
+        <Flex
+          direction='column'
+          width={{ base: "100%", md: "65%" }}
+          mr={{ base: 0, md: 6 }}
+          border='1px solid blue'
+        >
+          {children && children[0 as keyof typeof children]}
+        </Flex>
+        {/* RHS */}
+        <Flex
+          direction='column'
+          display={{ base: "none", md: "flex" }}
+          flexGrow={1}
+          border='1px solid orange'
+        >
+          {children && children[1 as keyof typeof children]}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+export default PageContent;
