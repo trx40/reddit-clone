797a5f9734d832b270243ffd116555cf60676791
trx40 <trm2468@gmail.com>
2023-02-07 23:02:36 +0530

    Update UserMenu.tsx
    Reset the communityState atom after the user has logged out

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -18,15 +18,22 @@ import {
 import { signOut, User } from "firebase/auth";
 import React from "react";
 import { auth } from "@/src/firebase/clientApp";
-import { useSetRecoilState } from "recoil";
+import { useResetRecoilState, useSetRecoilState } from "recoil";
 import { authModalState } from "@/src/atoms/authModalAtom";
+import { CommunityState } from "@/src/atoms/communitiesAtom";
 
 type UserMenuProps = {
   user?: User | null;
 };
 
 const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
+  const resetCommunityState = useResetRecoilState(CommunityState);
   const setAuthModalState = useSetRecoilState(authModalState);
+  const logout = async () => {
+    await signOut(auth);
+    //  reset communityState after Logout
+    resetCommunityState();
+  };
   return (
     <Menu>
       <MenuButton
@@ -89,7 +96,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
               fontSize='10pt'
               fontWeight={700}
               _hover={{ bg: "blue.500", color: "white" }}
-              onClick={() => signOut(auth)}
+              onClick={logout}
             >
               <Flex align='center'>
                 <Icon fontSize={20} as={MdOutlineLogin} mr={2} />
