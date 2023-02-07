a1dd36f46a7ee7931eca39351ae58f69cba1abe1
trx40 <trm2468@gmail.com>
2023-02-07 23:01:09 +0530

    Update useCommunityData.tsx
    Add functionality to fetch the community snippets for the user in our custom hook

--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilState } from "recoil";
-import { Community, CommunityState } from "../atoms/communitiesAtom";
+import {
+  Community,
+  CommunitySnippet,
+  CommunityState,
+} from "../atoms/communitiesAtom";
+import { auth, firestore } from "../firebase/clientApp";
 
 const useCommunityData = () => {
+  const [user] = useAuthState(auth);
   const [communityStateValue, setCommunityStateValue] =
     useRecoilState(CommunityState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const onJoinOrLeaveCommunity = (
     communityData: Community,
@@ -19,15 +29,38 @@ const useCommunityData = () => {
     }
     joinCommunity(communityData);
   };
+
+  const getMySnippets = async () => {
+    setLoading(true);
+    try {
+      //  get user's snippets
+      const snippetDocs = await getDocs(
+        collection(firestore, `users/${user?.uid}/communitySnippets`)
+      );
+      const snippets = snippetDocs.docs.map((doc) => ({ ...doc.data() }));
+      setCommunityStateValue((prev) => ({
+        ...prev,
+        mySnippets: snippets as Array<CommunitySnippet>,
+      }));
+    } catch (error) {
+      console.log("getMySnippets error", error);
+    }
+    setLoading(false);
+  };
+
   const joinCommunity = (communityData: Community) => {};
 
   const leaveCommunity = (communityId: string) => {};
-
+  useEffect(() => {
+    if (!user) return;
+    getMySnippets();
+  }, [user]);
   return {
     // data and functions
 
     communityStateValue,
     onJoinOrLeaveCommunity,
+    loading,
   };
 };
 export default useCommunityData;
