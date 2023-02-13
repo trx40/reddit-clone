143a9bdc6981c65229c5d4c3129fa6ea90df5ec8
trx40 <trm2468@gmail.com>
2023-02-13 19:34:37 +0530

    WIP - Submit post form

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,6 +1,19 @@
+import PageContent from "@/src/components/Layout/PageContent";
+import NewPostForm from "@/src/components/Posts/NewPostForm";
+import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 
 const SubmitPostPage: React.FC = () => {
-  return <div>Submit Post Page</div>;
+  return (
+    <PageContent>
+      <>
+        <Box p='14px 0px' borderBottom='1px solid white'>
+          <Text>Create a post</Text>
+        </Box>
+        <NewPostForm />
+      </>
+      <>{/* About */}</>
+    </PageContent>
+  );
 };
 export default SubmitPostPage;
