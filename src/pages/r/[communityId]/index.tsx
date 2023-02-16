3c0cebe0f6a69ecaf4af70a441545b84c8ee1839
trx40 <trm2468@gmail.com>
2023-02-16 21:29:38 +0530

    Set current community state when found

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,4 +1,5 @@
-import { Community } from "@/src/atoms/communitiesAtom";
+import { Community, CommunityState } from "@/src/atoms/communitiesAtom";
+import About from "@/src/components/Community/About";
 import CreatePostLink from "@/src/components/Community/CreatePostLink";
 import Header from "@/src/components/Community/Header";
 import CommunityNotFound from "@/src/components/Community/NotFound";
@@ -7,7 +8,8 @@ import Posts from "@/src/components/Posts/Posts";
 import { firestore } from "@/src/firebase/clientApp";
 import { doc, getDoc } from "firebase/firestore";
 import { GetServerSidePropsContext } from "next";
-import React from "react";
+import React, { useEffect } from "react";
+import { useSetRecoilState } from "recoil";
 import safeJSONStringify from "safe-json-stringify";
 
 type CommunityPageProps = {
@@ -15,6 +17,17 @@ type CommunityPageProps = {
 };
 
 const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  const setCommunityStateValue = useSetRecoilState(CommunityState);
+
+  useEffect(() => {
+    if (communityData) {
+      setCommunityStateValue((prev) => ({
+        ...prev,
+        currentCommunity: communityData,
+      }));
+    }
+  });
+
   if (!communityData) {
     return <CommunityNotFound />;
   }
@@ -27,7 +40,7 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
           <Posts communityData={communityData} />
         </>
         <>
-          <div>RHS</div>
+          <About communityData={communityData} />
         </>
       </PageContent>
     </>
