08e0f615fa508d0ba02db2a658c7afd735682769
trx40 <trm2468@gmail.com>
2023-02-17 22:13:17 +0530

    Update Header.tsx
    Display community header logo from state

--- src/components/Community/Header.tsx
+++ src/components/Community/Header.tsx
@@ -21,8 +21,17 @@ const Header: React.FC<HeaderProps> = ({ communityData }) => {
       <Box height='50%' bg='blue.400' />
       <Flex justify='center' bg='white' flexGrow={1}>
         <Flex width='95%' maxWidth='860px'>
-          {communityData.imageURL ? (
-            <Image alt='community logo' />
+          {communityStateValue.currentCommunity?.imageURL ? (
+            <Image
+              src={communityStateValue.currentCommunity.imageURL}
+              borderRadius='full'
+              boxSize='66px'
+              position='relative'
+              top={-3}
+              color='blue.500'
+              border='4px solid white'
+              alt='community logo'
+            />
           ) : (
             <Icon
               as={FaReddit}
