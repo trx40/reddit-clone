2624ed77e5ecfd3834225e212a51c8f974ff62f4
trx40 <trm2468@gmail.com>
2023-03-11 12:43:12 +0530

    PostPage blueprint created

--- src/pages/r/[communityId]/comments/[pid].tsx
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -1,6 +1,34 @@
+import PageContent from "@/src/components/Layout/PageContent";
+import PostItem from "@/src/components/Posts/PostItem";
+import { auth } from "@/src/firebase/clientApp";
+import usePosts from "@/src/hooks/usePosts";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const PostPage: React.FC = () => {
-  return <div>POST PAGE</div>;
+  const [user] = useAuthState(auth);
+  const { postStateValue, onDeletePost, onVote, setPostStateValue } =
+    usePosts();
+  return (
+    <PageContent>
+      <>
+        {postStateValue.selectedPost && (
+          <PostItem
+            post={postStateValue.selectedPost}
+            onVote={onVote}
+            onDeletePost={onDeletePost}
+            userVoteValue={
+              postStateValue.postVotes.find(
+                (item) => item.postId === postStateValue.selectedPost?.id
+              )?.voteValue
+            }
+            userIsCreator={user?.uid === postStateValue.selectedPost?.creatorId}
+          />
+        )}
+        {/* Comments */}
+      </>
+      <> {/* About */} </>
+    </PageContent>
+  );
 };
 export default PostPage;
