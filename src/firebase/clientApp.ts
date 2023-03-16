5a9fd709a0ac646dd83d1516c4c44b0f47cde30a
trx40 <trm2468@gmail.com>
2023-03-16 17:48:25 +0530

    Update clientApp.ts
    comments

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-import {getStorage} from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,11 +13,11 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase for server side rendering
-// If we use initiliazeApp as it is it will render twice, once at
+// If we use initiliazeApp as it is, it will render twice, once at
 // server and again at client due to how Next.js SSR works
 // To avoid that we check if apps do not exist
 // ?
@@ -29,4 +29,4 @@ const firestore = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
 
-export {app, firestore, auth, storage};
+export { app, firestore, auth, storage };
