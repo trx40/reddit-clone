87ff3570ea6c277131a2678c9d7d68e9b81ca154
trx40 <trm2468@gmail.com>
2023-03-18 22:16:37 +0530

    Update communitiesAtom.ts
    Add snippets fetched check to sync state

--- src/atoms/communitiesAtom.ts
+++ src/atoms/communitiesAtom.ts
@@ -1,31 +1,33 @@
-import { Timestamp } from "firebase/firestore"
-import {atom} from "recoil"
+import { Timestamp } from "firebase/firestore";
+import { atom } from "recoil";
 
 export interface Community {
-    id: string,
-    creatorId: string,
-    numberOfMembers: number,
-    privacyType: 'public' | 'restricted' | 'private',
-    createdAt?: Timestamp,
-    imageURL?: string,
+  id: string;
+  creatorId: string;
+  numberOfMembers: number;
+  privacyType: "public" | "restricted" | "private";
+  createdAt?: Timestamp;
+  imageURL?: string;
 }
 
 export interface CommunitySnippet {
-    communityId: string;
-    isModerator?: boolean;
-    imageURL?: string;
+  communityId: string;
+  isModerator?: boolean;
+  imageURL?: string;
 }
 
 interface CommunityState {
-    mySnippets: CommunitySnippet[];
-    currentCommunity?: Community;
+  mySnippets: CommunitySnippet[];
+  currentCommunity?: Community;
+  snippetsFetched: boolean;
 }
 
 const defaultCommunityState: CommunityState = {
-    mySnippets: []
-}
+  mySnippets: [],
+  snippetsFetched: false,
+};
 
-export const CommunityState= atom<CommunityState>({
-    key: 'communitiesState',
-    default: defaultCommunityState
-})
+export const CommunityState = atom<CommunityState>({
+  key: "communitiesState",
+  default: defaultCommunityState,
+});
